4abbe29335b12d439dd72362f228c21d904082fb
Jovamih <avira365@hotmail.com>
2020-04-28 18:40:20 -0500

    Se agrego test.py con la funcion principal

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include "utilities.h"
+using namespace std;
+int main(int argc, char **argv){
+
+    if(argc<2){
+        cout<<"Ingrese la ruta del archivo";
+    }else{
+
+    }
+
+    return 0;
+}
