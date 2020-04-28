40be03d479a31c4a119ce3975c9c014a14a553ae
Jovamih <avira365@hotmail.com>
2020-04-28 18:50:16 -0500

    Funcion principal mejorada

--- test.cpp
+++ test.cpp
@@ -8,6 +8,16 @@ int main(int argc, char **argv){
     if(argc<2){
         cout<<"Ingrese la ruta del archivo";
     }else{
+        string line;
+        ifstream data(argv[1]);
+        while(getline(data,line)){
+            Tokenizer tokenizer(line);
+            while(tokenizer.hasNextToken()){
+                Token token= tokenizer.nextToken();
+                cout<<token.type<<"\t"<<token.value<<endl;
+            }
+            
+        }
 
     }
 
