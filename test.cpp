d9424938c6baa0894519467cc9caa2a2645c0b5f
Jovamih <avira365@hotmail.com>
2020-04-30 16:32:07 -0500

    Se agrego un archivo externo para el manejo de palabras reservadas

--- test.cpp
+++ test.cpp
@@ -2,36 +2,52 @@
 #include<string>
 #include<fstream>
 #include "utilities.h"
+
+#define WORDS_SIZE 100
 using namespace std;
 void readReservedWords(string,string[]);
 bool isReservedWord(string,string*);
 int main(int argc, char **argv){
     if(argc<2){
-        cout<<"Ingrese el texto a Scanear";
+        cout<<"Ingrese la ruta del programa fuente";
         return -1;
     }
+    //1ero: leemos la ruta del archivo que tenemos que inspecionar
     fstream input(argv[1],ios::in);
-    string words[100];
+    //preparamos las palabras reservadas que se encuentran en otro archivo txt
+    string words[100]; //array de tokens leidos
     readReservedWords("Resources/wordsReserved.txt",words);
+    //verificamos si se abrio correctamente
     if(input.is_open()){
          string line;
-         while(getline(input,line)){
+         //comenzamos a leer LINEA por LINEA
+         while(getline(input,line,'\n')){
+             //cada linea es asignada a una clase Token QUE SE ENCARAGRA DE PROESARLA Y DETERMINAR
+             //cuantos Token  posee
              Tokenizer tokenizer(line);
+                //mientras haya un Token disponile
                 while(tokenizer.hasNextToken()){
+                    //leemos el token
                     Token token= tokenizer.nextToken();
-                    if(token.type=="ID"){
-                        if(isReservedWord(token.value,words)) token.type="PR";
+                    //SI el token devuelto no esta vacio, se procede a inspeccionar, en otro caso se ignora
+                   if(!token.value.empty()){
+                       //ademas si el token es de tipo 'ID' verficamos si perrtenece a una palabra reservada, y si no, pues lo omitimos
+                        if(token.type=="ID"){
+                            //si pertenece a una palabra reservada le cambiamos de tipo por un 'PR'
+                          if(isReservedWord(token.value,words)) token.type="PR";
+                    } 
+                    cout<<token.type<<"\t"<<token.value<<endl; //mostramos los datos
                     }
-                    cout<<token.type<<"\t"<<token.value<<endl;
-                }
+                 }
+                    
          }
-         input.close();
+         input.close();  
     }else{
         cout<<"No se puede acceder al archivo epecificado --argv[1]"<<endl;
     }
     return 0;
 }
-
+//Funcion que lee el archivo de palabras reservada y nos devuelve un array de String's
 void readReservedWords(string pathDirectory,string data[]){
     fstream input(pathDirectory,ios::in);
     if(input.is_open()){
@@ -45,8 +61,9 @@ void readReservedWords(string pathDirectory,string data[]){
         cout<<"No se encuentra el archivo de palabras reservadas --> wordsReserved.txt";
     }
 }
-bool isReservedWord(string value,string * data){
-    for(int i=0;data!=NULL;i++){
+//Funcion que verifica si un determinado valor de Token es una Palabra reservada
+bool isReservedWord(string value,string data[]){
+    for(int i=0;i<WORDS_SIZE;i++){
         if(data[i]==value) return true;
     }
     return false;
